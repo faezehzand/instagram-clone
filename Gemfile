05fb85533a417bf69f878dee0ed11423666e771d
faezehzand <faezezand1365@gmail.com>
2024-10-05 15:45:47 +0200

    creating new post

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "bootstrap", "~> 5.3.0"
 gem "sassc-rails"
 gem "devise"
 gem "font-awesome-sass", "~> 6.1.2"
+gem "activestorage-validator"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  # Ensure the images are present and of the correct content type
+  validates :images, presence: true
+  validate :acceptable_image
+
+  private
+
+  # Custom validation for image file type
+  def acceptable_image
+    images.each do |image|
+      unless image.content_type.start_with?("image/")
+        errors.add(:images, "must be an image")
+      end
+    end
+  end
 end
